c48c0f2c6fc09492e5ad72a27fc65271574bc229
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-12-12 22:45:27 +0000

    Add files via upload

--- htmltree.py
+++ htmltree.py
@@ -34,61 +34,8 @@ class nodes:
            
 
 
+
 def attributs(content,simbols):
-    ttrue=True
-    cursors=0
-    pos=0
-    posactual=0
-    posactual2=0
-    posactual3=0
-    returnlist=[]
-    chars=""
-    returnstring=""
-    while ttrue:
-        chars=""
-        posactual=-1
-        pos=-1
-        
-        for simbol in simbols:
-            posactual=content.find(simbol,cursors)
-            
-            if posactual!=-1:
-                
-                if posactual<pos or pos==-1:
-                    pos=posactual
-                    chars=simbol
-        if pos>-1:
-            if chars=="'" or chars=='"':
-                posactual3=content.find('"',pos+1)
-                posactual2=content.find("'",pos+1)
-                if posactual3<0:
-                    posactual3=posactual2
-                if posactual2<0:
-                    posactual2=posactual3
-                if posactual3<posactual2:
-                    posactual2=posactual3
-                if posactual2>-1:
-                    pos=posactual2+1
-                    
-            if 0==0:
-                returnstring=content[cursors:pos].strip()
-                if returnstring!="":
-                    
-                    returnlist=returnlist+[returnstring]
-                cursors=pos+1
-                if cursors>len(content)-1:
-                    cursors=len(content)-1
-                    ttrue=False 
-        else:
-             if cursors!=len(content)-1:
-               returnstring=content[cursors:].strip()
-            
-               returnlist=returnlist+[returnstring]
-             ttrue=False
-
-    return returnlist
-
-def aattributs(content,simbols):
     ttrue=True
     cursors=0
     pos=0
@@ -104,17 +51,12 @@ def aattributs(content,simbols):
         chars=""
         posactual=-1
         pos=-1
-        
         for simbol in simbols:
-            
             posactual=content.find(simbol,ons)
-            
             if posactual!=-1:
                 if posactual<=pos or pos==-1:
-                    
                     pos=posactual
                     chars=simbol
-        
         if pos>-1:
             
             if chars=="'" or chars=='"':
@@ -129,26 +71,23 @@ def aattributs(content,simbols):
                 if posactual2>-1:
                     pos=posactual2+1
                     ons=pos
-            
+                    
             
             
             if chars==simbols[0]:
                 returnstring=content[cursors:pos].strip()
-                if returnstring!="":
+                if 0==0:
                      returnstring=returnlist=returnlist+[returnstring]
+                     
                 cursors=pos+1
                     
                 ons=cursors
                 
-            
-            
-            
         else:
+            returnstring=content[cursors:].strip()
             
-            returnlist=returnlist+[content[cursors:].strip()]
-            
-           
-
+            if 0==0:
+                returnlist=returnlist+[returnstring]
             ttrue=False
     
     return returnlist
@@ -244,7 +183,7 @@ def processs(content):
     
     for tag in tags:
         l1=["<","'",'"']
-        listtags=aattributs(tag,l1)
+        listtags=attributs(tag,l1)
         
         count=0
         for ttag in listtags:
